55b9e00cb737eaf91b18c1ba8c2200b06735247e
Sina.f <sinaghahreman84@gmail.com>
2022-03-20 19:44:42 +0330

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     root.geometry(meta.get_geometry(root.winfo_screenwidth(), root.winfo_screenheight()))
     root.resizable(False, False)
 
-    if os.path.exists(meta.icon):
-        root.iconbitmap(default=meta.icon)
+    if os.path.exists(meta.icon_path):
+        root.iconphoto(True, tk.PhotoImage(file=meta.icon_path))
 
     app = model.App(root)
 
