f8a894b5a3f5ea05e8b86c7673782eab62af2c66
Sina.f <sina.programerr@gmail.com>
2022-05-01 18:27:12 +0430

    change 'get_geometry()' from 'meta'  to  'model'

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import meta
 if __name__ == "__main__":
     root = tk.Tk()
     root.title(meta.title)
-    root.geometry(meta.get_geometry(root.winfo_screenwidth(), root.winfo_screenheight()))
+    root.geometry(model.get_geometry(root.winfo_screenwidth(), root.winfo_screenheight()))
     root.resizable(False, False)
 
     if os.path.exists(meta.icon_path):
