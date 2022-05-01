b203eba107bf537469d5341bc35e7f923ac531b7
Sina.f <sina.programerr@gmail.com>
2022-05-01 18:23:38 +0430

    remove 'get_geometry()' from meta.py
    will be in model.py

--- meta.py
+++ meta.py
@@ -1,15 +1,6 @@
 import os
 
 
-def get_geometry(scr_width, scr_height, width=500, height=80):
-    scr_width = int(scr_width)
-    scr_height = int(scr_height)
-    start_width = int((scr_width / 2) - 250)
-    start_height = int((scr_height / 2) - 150)
-
-    return f"{width}x{height}+{start_width}+{start_height}"
-
-
 title = 'File Hider'
 main_root = os.getcwd()
 
